a5e30fdd1984b01550876655906269b401e55bbd
Tom Van Mele <vanmelet@ethz.ch>
2019-01-17 21:20:45 +0100

    error in setup file

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ optional_requirements = {}
 
 setup(
     name="compas_assembly",
-    version="version='0.1.1'",
+    version="0.1.1",
     description="Assembly data structures for the COMPAS framework.",
     long_description=long_description,
     long_description_content_type="text/markdown",
