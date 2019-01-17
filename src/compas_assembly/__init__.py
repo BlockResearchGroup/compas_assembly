65f4f24e82e7f76cfc1a0a14363c15ce2ac2684e
Tom Van Mele <vanmelet@ethz.ch>
2019-01-17 21:22:47 +0100

    Bump version to 0.1.3

--- src/compas_assembly/__init__.py
+++ src/compas_assembly/__init__.py
@@ -27,7 +27,7 @@ __copyright__ = "Block Research Group - ETH Zurich"
 __license__   = "MIT License"
 __email__     = "van.mele@arch.ethz.ch"
 
-__version__ = "__version__ = '0.1.2'"
+__version__ = "__version__ = '0.1.3'"
 
 
 HERE = os.path.dirname(__file__)
