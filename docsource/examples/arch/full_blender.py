0ef9b19b14556c9b0a1df332951c6909e4d42182
Tom Van Mele <vanmelet@ethz.ch>
2020-09-27 00:59:12 +0200

    cleaned up example

--- docsource/examples/arch/full_blender.py
+++ docsource/examples/arch/full_blender.py
@@ -1,3 +1,6 @@
+from random import randint
+
+import bpy
 import compas_blender
 
 from compas_assembly.datastructures import Assembly
@@ -5,7 +8,7 @@ from compas_assembly.datastructures import assembly_interfaces_numpy
 from compas_assembly.geometry import Arch
 from compas_assembly.blender import AssemblyArtist
 
-from compas_rbe.equilibrium import compute_interface_forces_cvx
+from compas_rbe.equilibrium import compute_interface_forces_cvxopt
 
 # ==============================================================================
 # Assembly
@@ -15,7 +18,7 @@ rise = 5
 span = 10
 depth = 0.5
 thickness = 0.7
-n = 40
+n = 15
 
 arch = Arch(rise, span, thickness, depth, n)
 assembly = Assembly.from_geometry(arch)
@@ -27,24 +30,26 @@ assembly.node_attribute(n - 1, 'is_support', True)
 # Identify the interfaces
 # ==============================================================================
 
-assembly_interfaces_numpy(assembly)
+assembly_interfaces_numpy(assembly, tmax=0.1, amin=0.1)
 
 # ==============================================================================
 # Compute interface forces
 # ==============================================================================
 
-compute_interface_forces_cvx(assembly, solver='CPLEX')
+compute_interface_forces_cvxopt(assembly)
 
 # ==============================================================================
 # Visualize
 # ==============================================================================
 
-compas_blender.delete_all_objects()
+compas_blender.clear()
 
-artist = AssemblyArtist(assembly, layer="Assembly")
+artist = AssemblyArtist(assembly)
 
 artist.draw_nodes()
 artist.draw_edges()
-artist.draw_blocks(show_faces=True, show_vertices=False, show_edges=False)
+#artist.draw_blocks()
 artist.draw_interfaces()
-artist.draw_resultants(scale=0.1)
+artist.draw_resultants(0.05)
+
+bpy.ops.object.select_all(action='DESELECT')
