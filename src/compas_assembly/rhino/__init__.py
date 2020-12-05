599d654554d66c26e72a5c0b66d55a38589fe784
Tom Van Mele <vanmelet@ethz.ch>
2020-12-05 22:15:15 +0100

    move helpers to objects module

--- src/compas_assembly/rhino/__init__.py
+++ src/compas_assembly/rhino/__init__.py
@@ -15,24 +15,14 @@ Artists
     BlockArtist
     AssemblyArtist
 
-Objects
-=======
-
-.. autosummary::
-    :toctree: generated/
-    :nosignatures:
-
-    BlockObject
-    AssemblyObject
-
 """
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from .helpers import *  # noqa: F401 F403
+# from .helpers import *  # noqa: F401 F403
 from .artists import *  # noqa: F401 F403
-from .objects import *  # noqa: F401 F403
+# from .objects import *  # noqa: F401 F403
 
 
 __all__ = [name for name in dir() if not name.startswith('_')]
