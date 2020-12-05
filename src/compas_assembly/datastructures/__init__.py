acdc77c385bc1341f2d915777a05a481ec38e5a8
Tom Van Mele <vanmelet@ethz.ch>
2020-12-05 10:46:56 +0100

    load interface at correct module level

--- src/compas_assembly/datastructures/__init__.py
+++ src/compas_assembly/datastructures/__init__.py
@@ -47,82 +47,79 @@ from __future__ import print_function
 
 import compas
 
-from .core import *
-from .assembly import Assembly
+from .core import *   # noqa: F401 F403
 
-from .hull import *
-
-if not compas.IPY:
-    from .hull_numpy import *
+from .assembly import Assembly   # noqa: F401 F403
+from .hull import *   # noqa: F401 F403
+from .transformations import *   # noqa: F401 F403
 
 if not compas.IPY:
-    from .interfaces_numpy import *
-
-from .transformations import *
-
-
-def assembly_interfaces_xfunc(data, **kwargs):
-    """Convenience wrapper for ``assembly_interfaces_numpy`` that can be used with ``XFunc`` or ``RPC``.
-
-    Parameters
-    ----------
-    data : dict
-        The data dictionary representing an assembly data structure.
-        The dict has the following items:
-
-        * assembly: the assembly data
-        * blocks: the data per block
-
-    Returns
-    -------
-    dict
-        A data dict with the same structure as the input dict.
-
-    Notes
-    -----
-    For additional named parameters that can be passed to this function,
-    see ``assembly_interfaces_numpy``.
-
-    Examples
-    --------
-    >>>
-    """
-    assembly = Assembly.from_data(data['assembly'])
-    assembly.blocks = {int(key): Block.from_data(data['blocks'][key]) for key in data['blocks']}
-    assembly_interfaces_numpy(assembly, **kwargs)
-    return {'assembly': assembly.to_data(),
-            'blocks': {str(key): assembly.blocks[key].to_data() for key in assembly.blocks}}
+    from .hull_numpy import *   # noqa: F401 F403
+    from .interfaces_numpy import *   # noqa: F401 F403
 
 
-def assembly_hull_xfunc(data, **kwargs):
-    """Convenience wrapper for ``assembly_hull_numpy`` that can be used with ``XFunc`` or ``RPC``.
+# def assembly_interfaces_xfunc(data, **kwargs):
+#     """Convenience wrapper for ``assembly_interfaces_numpy`` that can be used with ``XFunc`` or ``RPC``.
 
-    Parameters
-    ----------
-    data : dict
-        The data dictionary representing an assembly data structure.
-        The dict has the following items:
+#     Parameters
+#     ----------
+#     data : dict
+#         The data dictionary representing an assembly data structure.
+#         The dict has the following items:
 
-        * assembly: the assembly data
-        * blocks: the data per block
+#         * assembly: the assembly data
+#         * blocks: the data per block
 
-    Returns
-    -------
-    tuple
-        Vertices and faces of the hull.
+#     Returns
+#     -------
+#     dict
+#         A data dict with the same structure as the input dict.
 
-    Notes
-    -----
-    For additional named parameters that can be passed to this function,
-    see ``assembly_hull_numpy``.
+#     Notes
+#     -----
+#     For additional named parameters that can be passed to this function,
+#     see ``assembly_interfaces_numpy``.
 
-    Examples
-    --------
-    >>>
-    """
-    assembly = Assembly.from_data(data['assembly'])
-    assembly.blocks = {int(key): Block.from_data(data['blocks'][key]) for key in data['blocks']}
-    return assembly_hull_numpy(assembly, **kwargs)
+#     Examples
+#     --------
+#     >>>
+#     """
+#     assembly = Assembly.from_data(data['assembly'])
+#     assembly.blocks = {int(key): Block.from_data(data['blocks'][key]) for key in data['blocks']}
+#     assembly_interfaces_numpy(assembly, **kwargs)
+#     return {'assembly': assembly.to_data(),
+#             'blocks': {str(key): assembly.blocks[key].to_data() for key in assembly.blocks}}
+
+
+# def assembly_hull_xfunc(data, **kwargs):
+#     """Convenience wrapper for ``assembly_hull_numpy`` that can be used with ``XFunc`` or ``RPC``.
+
+#     Parameters
+#     ----------
+#     data : dict
+#         The data dictionary representing an assembly data structure.
+#         The dict has the following items:
+
+#         * assembly: the assembly data
+#         * blocks: the data per block
+
+#     Returns
+#     -------
+#     tuple
+#         Vertices and faces of the hull.
+
+#     Notes
+#     -----
+#     For additional named parameters that can be passed to this function,
+#     see ``assembly_hull_numpy``.
+
+#     Examples
+#     --------
+#     >>>
+#     """
+#     assembly = Assembly.from_data(data['assembly'])
+#     assembly.blocks = {int(key): Block.from_data(data['blocks'][key]) for key in data['blocks']}
+#     return assembly_hull_numpy(assembly, **kwargs)
 
 
 __all__ = [name for name in dir() if not name.startswith('_')]
--- src/compas_assembly/datastructures/core/__init__.py
+++ src/compas_assembly/datastructures/core/__init__.py
@@ -2,7 +2,8 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from .block import Block
-from .assembly import Assembly
+from .block import Block  # noqa: F401
+from .assembly import Assembly  # noqa: F401
+from .interface import Interface  # noqa: F401
 
 __all__ = [name for name in dir() if not name.startswith('_')]
