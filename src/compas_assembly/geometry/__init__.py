9c9bc7663aa27d889486569eabcecb32d66e34e6
brgcode <vanmelet@ethz.ch>
2021-08-23 21:21:08 +0200

    lint

--- src/compas_assembly/geometry/__init__.py
+++ src/compas_assembly/geometry/__init__.py
@@ -2,6 +2,6 @@ from __future__ import absolute_import
 
 import warnings
 
-from compas_assembly.templates import *
+from compas_assembly.templates import *  # noqa: F401 F403
 
 warnings.warn("Importing assembly templates from this module is deprecated. Use :mod:`compas_assembly.templates` instead.")
