55a4ba473b2352e11423c586f9a9401344eda6e8
brgcode <tom.v.mele@gmail.com>
2022-07-18 15:53:05 +0200

    add interface detection without numpy

--- src/compas_assembly/algorithms/__init__.py
+++ src/compas_assembly/algorithms/__init__.py
@@ -15,7 +15,9 @@ Functions
 
     assembly_hull
     assembly_hull_numpy
+    assembly_interfaces
     assembly_interfaces_numpy
+    mesh_mesh_interfaces
 
 """
 from __future__ import absolute_import
@@ -28,6 +30,12 @@ __all__ = ["assembly_hull"]
 
 if not compas.IPY:
     from .hull_numpy import assembly_hull_numpy
+    from .interfaces import assembly_interfaces
+    from .interfaces import mesh_mesh_interfaces
     from .interfaces_numpy import assembly_interfaces_numpy
 
-    __all__ += ["assembly_hull_numpy", "assembly_interfaces_numpy"]
+    __all__ += [
+        "assembly_hull_numpy",
+        "assembly_interfaces",
+        "assembly_interfaces_numpy",
+    ]
---
+++ src/compas_assembly/algorithms/interfaces.py
@@ -0,0 +1,146 @@
+from typing import List
+
+from math import fabs
+
+from shapely.geometry import Polygon as ShapelyPolygon
+
+from compas.geometry import Frame, Transformation
+from compas.geometry import centroid_polygon
+from compas.geometry import bestfit_frame_numpy, transform_points
+from compas.datastructures import Mesh
+
+from compas_assembly.datastructures import Assembly
+from compas_assembly.datastructures import Block
+from compas_assembly.datastructures import Interface
+from compas_assembly.algorithms.nnbrs import find_nearest_neighbours
+
+
+def assembly_interfaces(
+    assembly: Assembly,
+    nmax: int = 10,
+    tmax: float = 1e-6,
+    amin: float = 1e-1,
+):
+    """Identify the interfaces between the blocks of an assembly.
+
+    Parameters
+    ----------
+    assembly : compas_assembly.datastructures.Assembly
+        An assembly of discrete blocks.
+    nmax : int, optional
+        Maximum number of neighbours per block.
+    tmax : float, optional
+        Maximum deviation from the perfectly flat interface plane.
+    amin : float, optional
+        Minimum area of a "face-face" interface.
+
+    Returns
+    -------
+    :class:`Assembly`
+
+    """
+    node_index = {node: index for index, node in enumerate(assembly.nodes())}
+    index_node = {index: node for index, node in enumerate(assembly.nodes())}
+
+    blocks: List[Block] = list(assembly.blocks())
+
+    nmax = min(nmax, len(blocks))
+
+    block_cloud = [block.centroid() for block in blocks]
+    block_nnbrs = find_nearest_neighbours(block_cloud, nmax)
+
+    for node in assembly.nodes():
+        i = node_index[node]
+
+        block = blocks[i]
+        nbrs = block_nnbrs[i][1]
+
+        for j in nbrs:
+            n = index_node[j]
+
+            if n == node:
+                # a block has no interfaces with itself
+                continue
+
+            if assembly.graph.has_edge(n, node):
+                # the interfaces between these two blocks have already been identified
+                continue
+
+            nbr = blocks[j]
+
+            interfaces = mesh_mesh_interfaces(block, nbr, tmax, amin)
+
+            if interfaces:
+                assembly.add_block_block_interfaces(block, nbr, interfaces)
+
+
+def mesh_mesh_interfaces(
+    a: Mesh,
+    b: Mesh,
+    tmax: float = 1e-6,
+    amin: float = 1e-1,
+):
+    """Compute all face-face contact interfaces between two meshes.
+
+    Parameters
+    ----------
+    a : :class:`compas.datastructures.Mesh`
+    b : :class:`compas.datastructures.Mesh`
+    tmax : float, optional
+        Maximum deviation from the perfectly flat interface plane.
+    amin : float, optional
+        Minimum area of a "face-face" interface.
+
+    Returns
+    -------
+    List[:class:`Interface`]
+
+    """
+    world = Frame.worldXY()
+    interfaces = []
+
+    for face in a.faces():
+        points = a.face_coordinates(face)
+        result = bestfit_frame_numpy(points)
+        frame = Frame(*result)
+        matrix = Transformation.from_change_of_basis(world, frame)
+        projected = transform_points(points, matrix)
+        p0 = ShapelyPolygon(projected)
+
+        for test in b.faces():
+            points = b.face_coordinates(test)
+            projected = transform_points(points, matrix)
+            p1 = ShapelyPolygon(projected)
+
+            if not all(fabs(point[2]) < tmax for point in projected):
+                continue
+
+            if p1.area < amin:
+                continue
+
+            if not p0.intersects(p1):
+                continue
+
+            intersection = p0.intersection(p1)
+            area = intersection.area
+
+            if area < amin:
+                continue
+
+            coords = [[x, y, 0.0] for x, y, _ in intersection.exterior.coords]
+            coords = transform_points(coords, matrix.inverted())
+
+            interface = Interface(
+                type="face_face",
+                size=area,
+                points=coords,
+                frame=Frame(
+                    centroid_polygon(coords[:-1]),
+                    frame.xaxis,
+                    frame.yaxis,
+                ),
+            )
+
+            interfaces.append(interface)
+
+    return interfaces
