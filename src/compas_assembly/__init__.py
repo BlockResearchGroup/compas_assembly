59993095ff2c3b048550f3d29b3110d96bea4e0b
brgcode <tom.v.mele@gmail.com>
2022-11-09 00:43:44 +0100

    Bump version to 0.6.0

--- src/compas_assembly/__init__.py
+++ src/compas_assembly/__init__.py
@@ -28,7 +28,7 @@ __copyright__ = "Block Research Group - ETH Zurich"
 __license__ = "MIT License"
 __email__ = "van.mele@arch.ethz.ch"
 
-__version__ = "0.5.0"
+__version__ = "0.6.0"
 
 
 HERE = os.path.dirname(__file__)
